394291db62bb414b55c6b6364979a44c46480d6a
unknown <klfpaz@scl.edu.gt>
2025-02-25 19:02:21 -0600

    Funciones eliminar y resaltar

--- componentes/catalogo/funcionesProductos.js
+++ componentes/catalogo/funcionesProductos.js
@@ -2,7 +2,6 @@ import { dataProductos } from "./data.js";
 import { crearItemProducto } from "./itemProducto.js";  
 
 export function cargarProductos() {
-
     const catalogo = document.getElementById("root");
 
     if (catalogo) {
@@ -15,17 +14,23 @@ export function cargarProductos() {
 
         const ul = document.createElement("ul");
 
-       
         for (let i = 0; i < 10; i++) {
             const producto = dataProductos[i % dataProductos.length]; 
 
-           
             const itemProducto = crearItemProducto(producto);
+
+            // Añadir eventos para resaltar el producto al pasar el ratón
+            itemProducto.addEventListener("mouseover", function() {
+                itemProducto.classList.add("resaltado");
+            });
+
+            itemProducto.addEventListener("mouseout", function() {
+                itemProducto.classList.remove("resaltado");
+            });
 
             ul.appendChild(itemProducto);  
         }
 
-        
         catalogo.appendChild(ul);
     } else {
         console.error("No se encontró el contenedor 'root' para los productos.");
